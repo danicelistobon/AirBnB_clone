eaf5cbeac39d59a9bf1f45d8827fbbf298286fa8
danicelistobon <danielct-97@hotmail.com>
2019-11-13 00:52:54 +0000

    created console 0.1

--- console.py
+++ console.py
@@ -4,12 +4,15 @@
 
 
 import cmd
+import models
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class contains the entry point of the command interpreter
     """
     prompt = '(hbnb) '
+    classes = ['BaseModel']
 
     def do_quit(self, args):
         """Quit command to exit the program
@@ -25,6 +28,119 @@ class HBNBCommand(cmd.Cmd):
         """Does not execute anything when entering an empty line
         """
         pass
+
+    def do_create(self, args):
+        """Creates a new instance
+        """
+        arg = args.split()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+            return
+        elif arg[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        else:
+            obj = eval(arg[0])()
+            obj.save()
+            print(obj.id)
+
+    def do_show(self, args):
+        """Prints the string representation of an instance
+        """
+        arg = args.split()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+            return
+        elif arg[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        elif len(arg) == 1:
+            print("** instance id missing **")
+            return
+        else:
+            obj_id = str(arg[0] + "." + str(arg[1]))
+            objs = models.storage.all()
+            if obj_id not in objs.keys():
+                print("** no instance found **")
+                return
+            else:
+                print(objs[obj_id])
+
+    def do_destroy(self, args):
+        """Deletes an instance
+        """
+        arg = args.split()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+            return
+        elif arg[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        elif len(arg) == 1:
+            print("** instance id missing **")
+            return
+        else:
+            obj_id = str(arg[0] + "." + str(arg[1]))
+            objs = models.storage.all()
+            if obj_id not in objs.keys():
+                print("** no instance found **")
+                return
+            else:
+                del(objs[obj_id])
+                models.storage.save()
+
+    def do_all(self, args):
+        """Prints all string representation of all instances
+        """
+        arg = args.split()
+        objs = models.storage.all()
+        printer = []
+
+        if len(arg) == 0:
+            for value in objs.values():
+                printer.append(str(value))
+        elif arg[0] in self.classes:
+            for key, value in objs.items():
+                if arg[0] in key:
+                    printer.append(str(value))
+        else:
+            print("** class doesn't exist **")
+            return
+        print(printer)
+
+    def do_update(self, args):
+        """Updates an instance by adding or updating attribute
+        """
+        arg = args.split()
+
+        if len(arg) == 0:
+            print("** class name missing **")
+            return
+        elif arg[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+        elif len(arg) == 1:
+            print("** instance id missing **")
+            return
+        elif len(arg) == 2:
+            print("** attribute name missing **")
+            return
+        elif len(arg) == 3:
+            print("** value missing **")
+            return
+        else:
+            obj_id = str(arg[0] + "." + str(arg[1]))
+            objs = models.storage.all()
+            if obj_id not in objs.keys():
+                print("** no instance found **")
+                return
+            else:
+                obj = objs.get(obj_id)
+                setattr(obj, arg[2], arg[3][1:-1])
+                models.storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
