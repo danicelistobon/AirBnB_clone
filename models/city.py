7c4e2ee65bb2905d08463bb32010257822db4cbb
maryserar8903 <mary.serna.8903@gmail.com>
2019-11-13 00:48:04 +0000

    Created city

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to inherit from BaseModel
+"""
+
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class that inherits from BaseModel
+    """
+    state_id = ''
+    name = ''
